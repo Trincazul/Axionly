dac3da97e4c58f9e17f45764f11bdda5cfc505df
Endriw Villa <Endriw_villa@icloud.com>
2026-03-24 19:44:50 -0300

    Add base path configuration to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Axionly/", // <--- ADICIONE ESTA LINHA AQUI
+  base: "/Axionly/", 
   server: {
     host: "::",
     port: 8080,
